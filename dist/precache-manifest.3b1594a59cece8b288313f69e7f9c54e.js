a1d28d3cef95d18834e6c681dfcd96eceba5f372
algol007 <adhyformerz@gmail.com>
2020-05-13 18:26:29 +0800

    build 1.0

---
+++ dist/precache-manifest.3b1594a59cece8b288313f69e7f9c54e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56de9845c1eb87622547",
+    "url": "/css/app.bc91e728.css"
+  },
+  {
+    "revision": "d9fb3054e00c65841121da81c48e4fb1",
+    "url": "/fonts/AirbnbCereal-Medium.d9fb3054.ttf"
+  },
+  {
+    "revision": "52906cbeae8ff627182c5756046491d5",
+    "url": "/img/mollie-sivaram-_1gBVgy8gIU-unsplash.52906cbe.png"
+  },
+  {
+    "revision": "569ff00977a4a8928f4e8a6767b086ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56de9845c1eb87622547",
+    "url": "/js/app.b7b3418e.js"
+  },
+  {
+    "revision": "e7e271315461452787a5",
+    "url": "/js/chunk-vendors.5325b851.js"
+  },
+  {
+    "revision": "b069b7029e4d1adaa7bf326197cca0b6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
